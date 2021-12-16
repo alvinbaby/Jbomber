7921b566e7c889169deb86f0cb159b529018ded0
•JINN• <95747858+alvinbaby@users.noreply.github.com>
2021-12-16 19:35:07 +0530

    Update CyberGhostWeapon.sh

--- CyberGhostWeapon.sh
+++ CyberGhostWeapon.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 clear
-echo -e "\e[4;31m CyberGhost !!! \e[0m"
+echo -e "\e[4;31m Jbomber !!! \e[0m"
 echo -e "\e[1;34m Presents \e[0m"
-echo -e "\e[1;32m CyberGhostWeapon \e[0m"
+echo -e "\e[1;32m Jbomber \e[0m"
 echo "Press Enter To Continue"
 read a1
 if [[ -s update.speedx ]];then
@@ -14,7 +14,7 @@ echo .
 apt install figlet toilet python curl -y
 apt install python3-pip
 pip install -r requirements.txt
-echo This Script Was Made By CyberGhost >update.speedx
+echo This Script Was Made By Jbomber >update.speedx
 echo Requirements Installed....
 echo Press Enter To Continue...
 read upd
@@ -24,13 +24,13 @@ do
 rm *.xxx >/dev/null 2>&1
 clear
 echo -e "\e[1;31m"
-figlet CyberGhostWeapon
+figlet JbomberWeapon
 echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border CyberGhost
-echo -e "\e[4;34m This Bomber Was Created By CyberGhost\e[0m"
+toilet -f mono12 -F border Jbomber
+echo -e "\e[4;34m This Bomber Was Created By Jbomber\e[0m"
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: Cyb3r.gh02t.ha@gmail.com \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/CyberGhost \e[0m"
+echo -e "\e[1;32m           Mail: jinn@gmail.com \e[0m"
+echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/Jbomber \e[0m"
 echo " "
 echo -e "\e[4;31m Please Read Instruction Carefully !!! \e[0m"
 echo " "
@@ -66,9 +66,9 @@ cd ..
 rm -rf  TBomb >> temp
 rm update.speedx >> temp
 rm temp
-chmod +x CyberGhostWeapon.sh
+chmod +x JbomberWeapon.sh
 fi
-echo -e "\e[1;32m CyberGhostWeapon Will Restart Now..."
+echo -e "\e[1;32m JbomberWeapon Will Restart Now..."
 echo -e "\e[1;32m All The Required Packages Will Be Installed..."
 echo -e "\e[1;34m Press Enter To Proceed To Restart..."
 read a6
@@ -79,7 +79,7 @@ clear
 echo -e "\e[1;33m"
 figlet TBomb
 echo -e "\e[1;34mCreated By \e[1;34m"
-toilet -f mono12 -F border CyberGhost
+toilet -f mono12 -F border Jbomber
 echo  " "
 echo -e "\e[1;32m                   Features\e[1;34m"
 echo "  [+] Unlimited And Super-Fast Bombing"
@@ -106,22 +106,22 @@ echo -e "\e[1;32m Make Sure To Update it If It Does not Work.\e[0m"
 echo  " "
 echo -e "\e[4;31m That's All !!!\e[0m"
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail:cyb3r.gh02t.ha@gmail.com \e[0m"
+echo -e "\e[1;32m           Mail:jinn@gmail.com \e[0m"
 echo -e "\e[1;32m      Harsh Arora \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/CyberGhost\e[0m"
+echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/Jbomber\e[0m"
 echo "Press Enter To Go Home"
 read a3
 clear
 elif [ $ch -eq 5 ];then
 clear
 echo -e "\e[1;31m"
-figlet CyberGhostWeapon
+figlet JbomberWeapon
 echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border CyberGhost
+toilet -f mono12 -F border Jbomber
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: cyb3r.gh02t.ha@gmail.com \e[0m"
+echo -e "\e[1;32m           Mail: jinn@gmail.com \e[0m"
 echo -e "\e[1;32m        \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/CyberGhostWeapon \e[0m"
+echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/JbomberWeapon \e[0m"
 echo " "
 exit 0
 else
